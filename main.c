8de2a9274eff75d1e2b50e0c138c66e165c5a45a
cyjulien <julien.renard@etu.cyu.fr>
2025-05-05 16:19:57 +0200

    Updated main.c

--- main.c
+++ main.c
@@ -1,24 +1,37 @@
 #include "main.h"
 
 int main(int argc, char const *argv[]) {
-  srand(time(NULL));
+  srand(time(NULL)); // Initialize random seed
 
-  // Initialize teams
-  Team team1, team2;
+  Team player, enemy;
 
-  initializeChampion(&team1.champions[0], "Champion1_Team1");
-  initializeChampion(&team1.champions[1], "Champion2_Team1");
-  initializeChampion(&team1.champions[2], "Champion3_Team1");
+  // Initialize the player and enemy champions
+  //initializeChampion(&player.champions[0], "Player Champion");
+  //initializeChampion(&enemy.champions[0], "Enemy Champion");
+  /*Character chara = getCharacter("Johnson");
+  logCharacter(chara);*/
+  player.champions[0] = getCharacter("Example");
+  enemy.champions[0] = getCharacter("Johnson");
+  /*logCharacter(getCharacter("Example"));
+  logCharacter(player.champions[0]);*/
 
-  initializeChampion(&team2.champions[0], "Champion1_Team2");
-  initializeChampion(&team2.champions[1], "Champion2_Team2");
-  initializeChampion(&team2.champions[2], "Champion3_Team2");
+  int round = 1;
 
-  // Simulate turns
-  for (int turn = 1; turn <= 5; turn++) {
-      handleTurn(&team1, &team2, turn);
+  while (player.champions[0].HP > 0 && enemy.champions[0].HP > 0) {
+      handleRound(&player, &enemy, round);
+      round++;
   }
-  
+
+  // Check who won
+  if (player.champions[0].HP > 0) {
+      printf("You win!\n");
+  } else if (enemy.champions[0].HP > 0) {
+      printf("You lose! Enemy wins.\n");
+  } else {
+      printf("It's a draw!\n");
+  }
+  free(player.champions[0].name);
+  free(enemy.champions[0].name);
   printf("Execution ended.\n");
   return 0;
 }
