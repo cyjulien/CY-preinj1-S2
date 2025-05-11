1cb0deff31efc7369b2f5180a34ad038d7a7fb21
cyjulien <julien.renard@etu.cyu.fr>
2025-05-11 15:47:11 +0200

    Added end of combat

--- main.c
+++ main.c
@@ -10,20 +10,39 @@ int main(int argc, char const *argv[]) {
     Character allChars[MAX_TEAM_MEMBERS_COUNT * 2];
 
     int round = 1;
-    while (round < 100) {
+    int playersAlive = 3;
+    int enemiesAlive = 3;
+    
+    while (enemiesAlive > 0 && playersAlive > 0) {
         system("clear");
         system("cls");
 
+        playersAlive = 0;
         for (int i = 0; i < players.membersCount; i++) {
+            if (players.members[i].HP > 0) {
+                playersAlive++;
+            }
             allChars[i] = players.members[i];
         }
+        enemiesAlive = 0;
         for (int i = 0; i < enemies.membersCount; i++) {
+            if (enemies.members[i].HP > 0) {
+                enemiesAlive++;
+            }
             allChars[i + players.membersCount] = enemies.members[i];
         }
         char *dialogueMessage = NULL;
         displayFullInterface(players, enemies, dialogueMessage);
         handleRound(allChars, players, enemies, MAX_TEAM_MEMBERS_COUNT * 2, dialogueMessage);
     }
+    if (playersAlive == 0) {
+        printf("Enemies win!\n");
+    } else if (enemiesAlive == 0) {
+        printf("Players win!\n");
+    } else {
+        printf("Draw!\n");
+    }
+    
     for (int i = 0; i < players.membersCount; i++) {
         free(players.members[i].name);
         free(players.members[i].skills);
